7af1fa1f01ba93224fdb81e8e7b116e02dbd3a26
D1x1o <appleoygamer@gmail.com>
2026-03-11 16:25:57 +0300

    Реализовано открытие дампа базы данных

--- Kursovaya/Administrator/ImportExport.Designer.cs
+++ Kursovaya/Administrator/ImportExport.Designer.cs
@@ -30,58 +30,62 @@ namespace Kursovaya.Administrator
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormExportImport));
-            this.ExportButton = new System.Windows.Forms.Button();
+            this.OpenDumpButton = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.importButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // ExportButton
+            // OpenDumpButton
             // 
-            this.ExportButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(150)))), ((int)(((byte)(125)))));
-            this.ExportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ExportButton.Location = new System.Drawing.Point(450, 14);
-            this.ExportButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.ExportButton.Name = "ExportButton";
-            this.ExportButton.Size = new System.Drawing.Size(201, 49);
-            this.ExportButton.TabIndex = 0;
-            this.ExportButton.Text = "Экспортировать БД";
-            this.ExportButton.UseVisualStyleBackColor = false;
-            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            this.OpenDumpButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(150)))), ((int)(((byte)(125)))));
+            this.OpenDumpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenDumpButton.Location = new System.Drawing.Point(450, 14);
+            this.OpenDumpButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.OpenDumpButton.Name = "OpenDumpButton";
+            this.OpenDumpButton.Size = new System.Drawing.Size(201, 49);
+            this.OpenDumpButton.TabIndex = 0;
+            this.OpenDumpButton.Text = "Открыть";
+            this.OpenDumpButton.UseVisualStyleBackColor = false;
+            this.OpenDumpButton.Click += new System.EventHandler(this.OpenDumpButton_Click);
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(91)))), ((int)(((byte)(104)))));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
             this.richTextBox1.Location = new System.Drawing.Point(12, 71);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(849, 374);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
-            // button1
+            // importButton
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(150)))), ((int)(((byte)(125)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(659, 14);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(201, 49);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Экспортировать БД";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.importButton_Click);
+            this.importButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(150)))), ((int)(((byte)(125)))));
+            this.importButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.importButton.Location = new System.Drawing.Point(659, 14);
+            this.importButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(201, 49);
+            this.importButton.TabIndex = 0;
+            this.importButton.Text = "Импортировать БД";
+            this.importButton.UseVisualStyleBackColor = false;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
             // 
-            // button2
+            // exportButton
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(150)))), ((int)(((byte)(125)))));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(13, 14);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(201, 49);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Экспортировать БД";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.openDump);
+            this.exportButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(150)))), ((int)(((byte)(125)))));
+            this.exportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exportButton.Location = new System.Drawing.Point(13, 14);
+            this.exportButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(201, 49);
+            this.exportButton.TabIndex = 0;
+            this.exportButton.Text = "Экспортировать БД";
+            this.exportButton.UseVisualStyleBackColor = false;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
             // 
             // FormExportImport
             // 
@@ -90,9 +94,9 @@ namespace Kursovaya.Administrator
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(91)))), ((int)(((byte)(104)))));
             this.ClientSize = new System.Drawing.Size(873, 457);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.OpenDumpButton);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ForeColor = System.Drawing.Color.White;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -107,9 +111,9 @@ namespace Kursovaya.Administrator
 
         #endregion
 
-        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Button OpenDumpButton;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Button exportButton;
     }
 }
--- Kursovaya/Administrator/ImportExport.cs
+++ Kursovaya/Administrator/ImportExport.cs
@@ -18,21 +18,214 @@ namespace Kursovaya.Administrator
         public FormExportImport()
         {
             InitializeComponent();
+
         }
-
-        private void ExportButton_Click(object sender, EventArgs e)
+        public static void ExportMySQLDatabase(string connectionString)
         {
-            
-        }        
+            try
+            {
+                using (var folderDialog = new FolderBrowserDialog())
+                {
+                    folderDialog.Description = "Выберите папку для сохранения структуры и данных базы данных";
 
+                    if (folderDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        var builder = new MySqlConnectionStringBuilder(connectionString);
+                        string databaseName = builder.Database;
+
+                        if (string.IsNullOrEmpty(databaseName))
+                        {
+                            MessageBox.Show("Не удалось определить имя базы данных из строки подключения.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        string fileName = $"{databaseName}_backup_{DateTime.Now:yyyyMMdd_HHmmss}.sql";
+                        string fullPath = Path.Combine(folderDialog.SelectedPath, fileName);
+
+                        using (var connection = new MySqlConnection(connectionString))
+                        {
+                            connection.Open();
+
+                            using (var writer = new StreamWriter(fullPath))
+                            {
+                                writer.WriteLine($"-- MySQL Backup: {databaseName} - {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+                                writer.WriteLine("/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;\n");
+
+                                var tables = new List<string>();
+                                using (var cmd = new MySqlCommand($"SELECT table_name FROM information_schema.tables WHERE table_schema = '{databaseName}' AND table_type = 'BASE TABLE'", connection))
+                                using (var reader = cmd.ExecuteReader())
+                                {
+                                    while (reader.Read()) tables.Add(reader.GetString(0));
+                                }
+
+                                foreach (var table in tables)
+                                {
+                                    // Структура
+                                    using (var cmd = new MySqlCommand($"SHOW CREATE TABLE `{databaseName}`.`{table}`", connection))
+                                    using (var reader = cmd.ExecuteReader())
+                                    {
+                                        if (reader.Read())
+                                        {
+                                            writer.WriteLine($"-- Table: {table}");
+                                            writer.WriteLine($"DROP TABLE IF EXISTS `{table}`;");
+                                            writer.WriteLine(reader.GetString(1) + ";\n");
+                                        }
+                                    }
+
+                                    // Данные
+                                    using (var cmd = new MySqlCommand($"SELECT * FROM `{databaseName}`.`{table}`", connection))
+                                    using (var reader = cmd.ExecuteReader())
+                                    {
+                                        var columns = Enumerable.Range(0, reader.FieldCount).Select(i => reader.GetName(i)).ToArray();
+
+                                        while (reader.Read())
+                                        {
+                                            writer.Write($"INSERT INTO `{table}` ({string.Join(", ", columns.Select(c => $"`{c}`"))}) VALUES (");
+
+                                            for (int i = 0; i < reader.FieldCount; i++)
+                                            {
+                                                if (i > 0) writer.Write(", ");
+
+                                                if (reader.IsDBNull(i))
+                                                    writer.Write("NULL");
+                                                else
+                                                {
+                                                    var val = reader.GetValue(i);
+                                                    if (val is string || val is DateTime || val is TimeSpan)
+                                                        writer.Write($"'{val.ToString().Replace("'", "''")}'");
+                                                    else if (val is bool)
+                                                        writer.Write((bool)val ? "1" : "0");
+                                                    else if (val is byte[])
+                                                        writer.Write($"0x{BitConverter.ToString((byte[])val).Replace("-", "")}");
+                                                    else
+                                                        writer.Write(val);
+                                                }
+                                            }
+                                            writer.WriteLine(");");
+                                        }
+                                    }
+                                    writer.WriteLine();
+                                }
+
+                                writer.WriteLine("/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;");
+                                writer.WriteLine($"-- Backup completed: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+                            }
+                        }
+
+                        MessageBox.Show($"База данных экспортирована:\n{fullPath}", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
         private void importButton_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void openDump(object sender, EventArgs e)
+        private void OpenDumpButton_Click(object sender, EventArgs e)
         {
+            OpenSQLFileInRichTextBoxAdvanced(richTextBox1);
+        }
 
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            ExportMySQLDatabase(connStr);
+        }
+        public static void OpenSQLFileInRichTextBoxAdvanced(RichTextBox richTextBox1)
+        {
+            try
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Title = "Выберите SQL файл для загрузки";
+                    openFileDialog.Filter = "SQL files (*.sql)|*.sql|SQL Backup files (*.bak)|*.bak|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 1;
+                    openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string filePath = openFileDialog.FileName;
+                        FileInfo fileInfo = new FileInfo(filePath);
+
+                        // Проверяем размер файла (если больше 10 MB, предупреждаем)
+                        if (fileInfo.Length > 10 * 1024 * 1024)
+                        {
+                            var result = MessageBox.Show("Файл очень большой. Загрузка может занять некоторое время. Продолжить?",
+                                "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                            if (result == DialogResult.No)
+                                return;
+                        }
+
+                        // Показываем индикатор загрузки (если есть)
+                        // progressBar1.Visible = true;
+
+                        // Читаем файл с определением кодировки
+                        string fileContent = ReadFileWithEncoding(filePath);
+
+                        // Обновляем RichTextBox
+                        richTextBox1.Clear();
+                        richTextBox1.Text = fileContent;
+
+                        // Применяем форматирование
+                        richTextBox1.SelectAll();
+                        richTextBox1.SelectionFont = new System.Drawing.Font("Consolas", 10);
+                        richTextBox1.Select(0, 0);
+
+                        // Добавляем информацию о файле в начало (опционально)
+                        string header = $"-- Файл: {fileInfo.Name}\r\n-- Размер: {fileInfo.Length:N0} байт\r\n-- Загружен: {DateTime.Now:yyyy-MM-dd HH:mm:ss}\r\n-- Количество строк: {fileContent.Split('\n').Length}\r\n\r\n";
+
+                        // Вставляем заголовок, если его еще нет
+                        if (!richTextBox1.Text.StartsWith("-- Файл:"))
+                        {
+                            richTextBox1.Select(0, 0);
+                            richTextBox1.SelectedText = header;
+                        }
+
+                        // Прокручиваем в начало
+                        richTextBox1.SelectionStart = 0;
+                        richTextBox1.ScrollToCaret();
+
+                        // Скрываем индикатор загрузки
+                        // progressBar1.Visible = false;
+
+                        MessageBox.Show($"Файл успешно загружен:\n{fileInfo.Name}\nРазмер: {fileInfo.Length:N0} байт\nСтрок: {fileContent.Split('\n').Length}",
+                            "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при открытии файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Вспомогательная функция для чтения файла с определением кодировки
+        private static string ReadFileWithEncoding(string filePath)
+        {
+            try
+            {
+                // Пробуем прочитать с UTF-8
+                return File.ReadAllText(filePath, System.Text.Encoding.UTF8);
+            }
+            catch
+            {
+                try
+                {
+                    // Если не получилось, пробуем с ANSI
+                    return File.ReadAllText(filePath, System.Text.Encoding.GetEncoding(1251));
+                }
+                catch
+                {
+                    // Если всё еще не получилось, читаем как есть
+                    return File.ReadAllText(filePath);
+                }
+            }
         }
     }
 }
