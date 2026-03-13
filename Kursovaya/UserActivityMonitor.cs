68998e7952ac4be418e22c521508f58df8eee405
D1x1o <appleoygamer@gmail.com>
2026-03-13 11:52:08 +0300

    Реализован таймаут неактивности. Closes #4

---
+++ Kursovaya/UserActivityMonitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kursovaya
+{
+    public static class UserActivityMonitor
+    {
+        public static DateTime LastActivity = DateTime.Now;
+        public static int TimeoutSeconds = 30;
+        private static Timer timer;
+        public static void Start()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            Application.AddMessageFilter(new ActivityMessageFilter());
+        }
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            if((DateTime.Now - LastActivity).TotalSeconds > TimeoutSeconds)
+            {
+                timer.Stop();
+                MessageBox.Show("Сессия истекла из за неактивности", "Сессия", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                foreach(Form f in Application.OpenForms)
+                {
+                    if (!(f is Auth)) f.Hide();
+                }
+                new Auth().ShowDialog();
+            }
+        }
+    }
+}
